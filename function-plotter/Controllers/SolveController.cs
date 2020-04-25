fec248a80989408aebc28959b9040d7ca2728cc7
Petru <martincu.petru@gmail.com>
2020-04-25 17:11:09 +0300

    Added endpoint to return the equation array of results

---
+++ function-plotter/Controllers/SolveController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Threading.Tasks;
+using function_plotter.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace function_plotter.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SolveController : ControllerBase
+    {
+        [HttpGet]
+        public IList<Pair> Get()
+        {
+            var response = new List<Pair>();
+            response.Add(new Pair(0, 1));
+            response.Add(new Pair(1, 2));
+            response.Add(new Pair(2, 3));
+            response.Add(new Pair(3, 4));
+            response.Add(new Pair(4, 5));
+
+            return response;
+        }
+    }
+}

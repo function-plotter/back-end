855118723e55a804032a4808ac3fa20d0f8f5194
mircearares <mirceararesgabriel@gmail.com>
2020-04-25 21:18:11 +0300

    Refactored some code

--- function-plotter/Models/FunctionPlotter.cs
+++ function-plotter/Models/FunctionPlotter.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace function_plotter.Models
+﻿namespace function_plotter.Models
 {
     public class FunctionPlotter
     {
         public Function Function { get; set; }
-        public Pair Interval { get; set; }
-        public double Step { get; set; }
+        public Range Range { get; set; }
     }
 }
---
+++ function-plotter/Solvers/Solver.cs
@@ -0,0 +1,21 @@
+﻿using function_plotter.Models;
+using System.Collections.Generic;
+
+namespace function_plotter.Solvers
+{
+    public class Solver
+    {
+        private readonly FunctionPlotter _functionSolver;
+
+        public Solver(FunctionPlotter functionPlotter)
+        {
+            _functionSolver = functionPlotter;
+        }
+
+        public List<Pair> Solve()
+        {
+            // facut ceva pentru functii, altceva pentru integrale ??
+            return new List<Pair>();
+        }
+    }
+}

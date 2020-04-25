51da2576612da0bd0305c17f4e2884b6d741aa14
Petru <martincu.petru@gmail.com>
2020-04-25 17:10:26 +0300

    Added model for (x, y) pair

---
+++ function-plotter/Models/Pair.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace function_plotter.Models
+{
+    public class Pair
+    {
+        public Pair(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+}

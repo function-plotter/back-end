f7de8c91eeeae98637d92e8c213d685554aff3c9
mircearares <mirceararesgabriel@gmail.com>
2020-04-25 17:55:56 +0300

    Added model for Function and Operator type

---
+++ function-plotter/Models/FunctionType.cs
@@ -0,0 +1,15 @@
+﻿namespace function_plotter.Models
+{
+    public enum FunctionType
+    {
+        Var,
+        Const,
+        Add,
+        Mult,
+        Div,
+        Sin,
+        Cos,
+        Pow,
+        Integral
+    }
+}

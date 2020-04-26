ea185ebed7ca400d34b8afcb13f055b4c9aef502
Petru <martincu.petru@gmail.com>
2020-04-26 13:36:13 +0300

    Added NUnit Tests for Solver

---
+++ funtion-plotter.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace funtion_plotter.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
